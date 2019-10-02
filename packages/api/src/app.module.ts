f6affd562fab07d91cfad7f4a19bf8b40af31e33
Federico Marcos <federico.marcos@celerative.com>
2019-10-01 22:43:44 -0300

    initial commit

---
+++ packages/api/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
